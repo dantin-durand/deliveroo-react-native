52fd280800dc82578c8d03e674001ac19ac2d43e
Dantin Durand <redvinyle@MacBook-Pro-de-Dantin.home>
2021-12-12 23:06:12 +0100

    [fix] share element transition

--- src/screens/restaurantScreen.js
+++ src/screens/restaurantScreen.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {Button, StyleSheet, Text, View, ImageBackground} from 'react-native';
 
 import {DefaultLayout} from '../layouts/default';
+import {SharedElement} from 'react-navigation-shared-element';
+
 import {SubTitle} from '../components/atoms/Texts/SubTitle';
 Restaurant.sharedElements = (route, otherRoute, showing) => {
   console.log('route -> ', route);
@@ -11,17 +13,19 @@ Restaurant.sharedElements = (route, otherRoute, showing) => {
 };
 
 function Restaurant(props) {
-  console.log('test');
   const {item} = props.route.params;
   return (
     <DefaultLayout>
       <View style={styles.sectionCategories}>
-        <ImageBackground
-          source={{
-            uri: item.image,
-          }}
-          resizeMode="cover"
-          style={{height: 200, width: '100%'}}></ImageBackground>
+        <SharedElement id={`item.${item.id}`}>
+          <ImageBackground
+            source={{
+              uri: item.image,
+            }}
+            resizeMode="cover"
+            style={{height: 200, width: '100%'}}
+          />
+        </SharedElement>
         <View style={styles.container}>
           <SubTitle>{item.name}</SubTitle>
         </View>
